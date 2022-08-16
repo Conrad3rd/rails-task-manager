8598b6167cc9c22cbdbab3155bb580a75bb1b7e5
Conrad Esche <conrad.3rd@gmail.com>
2022-08-16 15:19:52 +0200

    rails task manager: As a user, I can add a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,20 @@ class TasksController < ApplicationController
     # raise
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
