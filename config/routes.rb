c50913164d3e988032457cba06586c724b34f1fc
Conrad Esche <conrad.3rd@gmail.com>
2022-08-16 16:15:50 +0200

    4. As a user, I can edit a task (mark as completed / update title & details)

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   get 'tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#new'
-  get 'tasks/:id', to: 'tasks#show', as: :task
   post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update', as: :update
 end
